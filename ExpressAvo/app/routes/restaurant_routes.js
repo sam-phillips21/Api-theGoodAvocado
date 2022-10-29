1a32eb09dfc747b3033533a7cb2161009080a498
melanie silva <melsil11@hotmail.com>
2022-10-29 11:42:46 -0400

    created routes fro restaurant and reviews and added them to the server.js file

---
+++ ExpressAvo/app/routes/restaurant_routes.js
@@ -0,0 +1,112 @@
+// Express docs: http://expressjs.com/en/api.html
+const express = require('express')
+// Passport docs: http://www.passportjs.org/docs/
+const passport = require('passport')
+
+// pull in Mongoose model for restaurants
+const Restaurant = require('../models/restaurant')
+
+// this is a collection of methods that help us detect situations when we need
+// to throw a custom error
+const customErrors = require('../../lib/custom_errors')
+
+// we'll use this function to send 404 when non-existant document is requested
+const handle404 = customErrors.handle404
+// we'll use this function to send 401 when a user tries to modify a resource
+// that's owned by someone else
+const requireOwnership = customErrors.requireOwnership
+
+// this is middleware that will remove blank fields from `req.body`, e.g.
+// { restaurant: { title: '', text: 'foo' } } -> { restaurant: { text: 'foo' } }
+const removeBlanks = require('../../lib/remove_blank_fields')
+// passing this as a second argument to `router.<verb>` will make it
+// so that a token MUST be passed for that route to be available
+// it will also set `req.user`
+const requireToken = passport.authenticate('bearer', { session: false })
+
+// instantiate a router (mini app that only handles routes)
+const router = express.Router()
+
+//* INDEX
+//* /restaurants
+router.get('/restaurants', (req, res, next) => {
+    Restaurant.find()
+        .populate('owner')
+        .then(restaurants => {
+            return restaurants.map(restaurant => restaurant)
+        })
+        .then(restaurants => {
+            res.status(200).json({ restaurants: restaurants })
+        })
+        .catch(next)
+})
+
+//* SHOW
+//* /restaurants/:id
+router.get('/restaurants/:id', (req, res, next) => {
+    Restaurant.findById(req.params.id)
+        .populate('owner')
+        .then(handle404)
+        .then(restaurant => {
+            res.status(200).json({ restaurant: restaurant})
+        })
+        .catch(next)
+})
+
+//* CREATE
+//* /restaruants
+router.post('/restaruants', requireToken, (req, res, next) => {
+    req.body.restaurant.owner = req.user.id
+
+    // on the front end, I HAVE to send a restaurant as the top level key
+    Restaurant.create(req.body.restaurant)
+    .then(restaurant => {
+        res.status(201).json({ restaurant: restaurant })
+    })
+    .catch(next)
+    // ^^^ shorthand for:
+        //^ .catch(error => next(error))
+})
+
+// UPDATE
+// PATCH /restaurants/5a7db6c74d55bc51bdf39793
+router.patch('/restaurants/:id', requireToken, removeBlanks, (req, res, next) => {
+	// if the client attempts to change the `owner` property by including a new
+	// owner, prevent that by deleting that key/value pair
+	delete req.body.restaurant.owner
+
+	Restaurant.findById(req.params.id)
+		.then(handle404)
+		.then((restaurant) => {
+			// pass the `req` object and the Mongoose record to `requireOwnership`
+			// it will throw an error if the current user isn't the owner
+			requireOwnership(req, restaurant)
+
+			// pass the result of Mongoose's `.update` to the next `.then`
+			return restaurant.updateOne(req.body.restaurant)
+		})
+		// if that succeeded, return 204 and no JSON
+		.then(() => res.sendStatus(204))
+		// if an error occurs, pass it to the handler
+		.catch(next)
+})
+
+
+//* DESTROY
+router.delete('/restaurants/:id', requireToken, (req, res, next) => {
+	Restaurant.findById(req.params.id)
+		.then(handle404)
+		.then((restaurant) => {
+			// throw an error if current user doesn't own `restaurant`
+			requireOwnership(req, restaurant)
+			// delete the restaurant ONLY IF the above didn't throw
+			restaurant.deleteOne()
+		})
+		// send back 204 and no content if the deletion succeeded
+		.then(() => res.sendStatus(204))
+		// if an error occurs, pass it to the handler
+		.catch(next)
+})
+
+
+module.exports = router
