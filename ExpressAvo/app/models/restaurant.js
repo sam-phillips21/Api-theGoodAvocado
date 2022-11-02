b044a6799faa19923456ccdc5341cc477efcbc62
sam-phillips21 <phillips.sam@gmail.com>
2022-11-02 08:11:52 -0600

    vegan updates

--- ExpressAvo/app/models/restaurant.js
+++ ExpressAvo/app/models/restaurant.js
@@ -63,6 +63,10 @@ const restaurantSchema = new mongoose.Schema(
 			type: Boolean,
 			default: false
 		},
+		veganOptions: {
+			type: Boolean,
+			default: false
+		},
 
 		reviews: [reviewSchema],
 		owner: {
