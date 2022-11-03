493a9060360e1c56c490eb8078864f509ef1f35c
Angel Zhou <angel.zhou1016@gmail.com>
2022-11-02 20:26:57 -0400

    adds username virtual

--- ExpressAvo/app/models/review.js
+++ ExpressAvo/app/models/review.js
@@ -38,6 +38,10 @@ reviewSchema.virtual('highlyRecommend').get(function () {
     }
 })
 
+reviewSchema.virtual('username').get(function () {
+	return this.ownerEmail.slice(0, this.ownerEmail.indexOf('@'))
+})
+
 /////////////////////////////////////////////
 // Export Schema
 /////////////////////////////////////////////
